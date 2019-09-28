41862f8868405efb53743cea1ebdc606999550ac
XBCY <809382461@qq.com>
2019-09-28 19:32:19 +0800

    [FUN] 修改封面

--- startScene.py
+++ startScene.py
@@ -12,17 +12,17 @@ class StartScene:
 
     def __init__(self):
         """ 初始化 """
-        # 封面图由两张图组成，尺寸相同，coverImage2为闭眼状态
         coverImage = pygame.image.load('src/coverImage.png').convert()
-        coverImage.set_colorkey(Settings.defaultColorKey)
         self._coverImages = baseFunc.divideSruface(coverImage, 1, 2)
+        self._textImage = pygame.image.load('src/coverText.png').convert()
 
     def draw(self, screen):
         """ 绘制 """
         screen.fill((255, 255, 255))
-        left = round((Settings.initialWindowSize[0] - self._coverImages[0].get_width()) / 2)
-        top = round((Settings.initialWindowSize[1] - self._coverImages[0].get_height()) / 2)
-        if 0 <= pygame.time.get_ticks() % 5000 <= 100:
-            screen.blit(self._coverImages[1], (left, top))
-        else:
-            screen.blit(self._coverImages[0], (left, top))
+        totalWidth = self._textImage.get_width()
+        totalHeight = self._coverImages[0].get_height() * 2 + self._textImage.get_height()
+        left = round((Settings.initialWindowSize[0] - totalWidth) / 2)
+        top = round((Settings.initialWindowSize[1] - totalHeight) / 2)
+        screen.blit(self._coverImages[0 <= pygame.time.get_ticks() % 5000 <= 100], (left, top))
+        top += self._coverImages[0].get_height() * 2
+        screen.blit(self._textImage, (left, top))
